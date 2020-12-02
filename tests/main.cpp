b46edb427773de356ca8e7097914aa8dbdc099d2
Samuel Béland <samuel.beland@gmail.com>
2020-12-02 14:47:41 -0500

    Better automated benchmarking.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,42 +3,35 @@
 
 #include <resources/resources.hpp>
 
-TEST_CASE("day 1 a")
+TEST_CASE("day_1_a")
 {
     REQUIRE(day_1_a(inputs::TEST_1_A_1) == "514579");
     REQUIRE(day_1_a(inputs::DAY_1) == "921504");
-
-#ifdef NDEBUG
-    BENCHMARK("day 1 a") { day_1_a(inputs::DAY_1); };
-#endif
 }
 
-TEST_CASE("day 1 b")
+TEST_CASE("day_1_b")
 {
     REQUIRE(day_1_b(inputs::TEST_1_A_1) == "241861950");
     REQUIRE(day_1_b(inputs::DAY_1) == "195700142");
-
-#ifdef NDEBUG
-    BENCHMARK("day 1 b") { day_1_b(inputs::DAY_1); };
-#endif
 }
 
-TEST_CASE("day 2 a")
+TEST_CASE("day_2_a")
 {
     REQUIRE(day_2_a(inputs::TEST_2_A_1) == "2");
     REQUIRE(day_2_a(inputs::DAY_2) == "467");
-
-#ifdef NDEBUG
-    BENCHMARK("day 2 a") { day_2_a(inputs::DAY_2); };
-#endif
 }
 
-TEST_CASE("day 2 b")
+TEST_CASE("day_2_b")
 {
     REQUIRE(day_2_b(inputs::TEST_2_A_1) == "1");
     REQUIRE(day_2_b(inputs::DAY_2) == "441");
+}
 
 #ifdef NDEBUG
-    BENCHMARK("day 2 b") { day_2_b(inputs::DAY_2); };
-#endif
+TEST_CASE("Benchmarks")
+{
+    for (auto const & day : DAYS) {
+        BENCHMARK(day.name) { return day.function(); };
+    }
 }
+#endif
