51f731c44e097546c256de0239dbb6860e9b7e1c
Samuel Béland <samuel.beland@gmail.com>
2020-12-02 00:33:36 -0500

    Base files for DAY 2.

---
+++ src/day_2.cpp
@@ -0,0 +1,11 @@
+#include <resources/resources.hpp>
+
+std::string day_2_a(char const * input_file_path)
+{
+    return "coucou";
+}
+
+std::string day_2_b(char const * input_file_path)
+{
+    return "coucou";
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -14,3 +14,8 @@ TEST_CASE("day 1 b")
     REQUIRE(day_1_b(inputs::TEST_1_A_1) == "241861950");
     REQUIRE(day_1_b(inputs::DAY_1) == "195700142");
 }
+
+TEST_CASE("day 2 a")
+{
+    REQUIRE(day_2_a(inputs::TEST_1_A_1) == "2");
+}
