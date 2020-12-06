05285aa6ee17ea10668f4a01fc90f58120c31c97
Samuel Béland <samuel.beland@gmail.com>
2020-12-06 00:21:30 -0500

    Added descriptions for dayy 4-5 and resources for day 6.

---
+++ src/day_6.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+#include <resources.hpp>
+
+//==============================================================================
+std::string day_6_a(char const * input_file_path)
+{
+    return "day_6_a not implemented";
+}
+
+//==============================================================================
+std::string day_6_b(char const * input_file_path)
+{
+    return "day_6_b not implemented";
+}
