97a69b82d44310763e7a1e1d71b9f234f074ea14
Samuel Béland <samuel.beland@gmail.com>
2020-12-08 22:28:44 -0500

    Added base files for day 8.

---
+++ src/day_8.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+#include <resources.hpp>
+
+//==============================================================================
+std::string day_8_a(char const * input_file_path)
+{
+    return "day_8_a not implemented!";
+}
+
+//==============================================================================
+std::string day_8_b(char const * input_file_path)
+{
+    return "day_8_b not implemented!";
+}
