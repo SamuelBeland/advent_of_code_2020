e0aa09c255892b5cf4d7049d446eff601e6d72d4
Samuel Béland <samuel.beland@gmail.com>
2020-12-16 21:24:34 -0500

    Added base files for day 12.

---
+++ src/day_12.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+#include <resources.hpp>
+
+//==============================================================================
+std::string day_12_a(char const * input_file_path)
+{
+    return "day_12_a not implemented";
+}
+
+//==============================================================================
+std::string day_12_b(char const * input_file_path)
+{
+    return "day_12_b not implemented";
+}
