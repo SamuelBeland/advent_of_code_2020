b2a2d15d039a71088fc5645249901c9ebaf3ed8b
Samuel Béland <samuel.beland@gmail.com>
2020-12-23 23:32:59 -0500

    Added base files for day 15 and description for day 14.

---
+++ src/day_15.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+
+#include "utils.hpp"
+#include <resources.hpp>
+
+//==============================================================================
+std::string day_15_a(char const * input_file_path)
+{
+    return std::to_string(666);
+}
+
+//==============================================================================
+std::string day_15_b(char const * input_file_path)
+{
+    return std::to_string(666);
+}
