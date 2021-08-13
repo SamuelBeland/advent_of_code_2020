1fa1b0259fe994aeea59ab3da45644b6f57332f1
Samuel Béland <samuel.beland@gmail.com>
2021-08-13 13:24:07 -0400

    Added base files for day 17.

---
+++ src/day_17.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+#include <resources.hpp>
+
+//==============================================================================
+std::string day_17_a(char const * input_file_path)
+{
+    return "coucou";
+}
+
+//==============================================================================
+std::string day_17_b(char const * input_file_path)
+{
+    return "coucou";
+}
